800b1d32bfb00d9051d5ff7bff42fb0c4ae13f39
Chris Moran <cmoran@hbs.edu>
2022-01-10 17:41:44 -0500

    add github links
    might not work

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # Copy  rastertoLogia  /usr/lib/cups/filter/
 # Copy  Logia.ppd.gz   /usr/share/ppd
-
+curl -LOJ https://github.com/infestacool/Logia_linux/blob/main/Logia.ppd.gz
+curl -LOJ https://github.com/infestacool/Logia_linux/blob/main/rastertoLogia
 cp rastertoLogia  /usr/lib/cups/filter/
 cp Logia.ppd.gz   /usr/share/ppd
 
